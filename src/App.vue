d0978fabe8992cfda37eee29938bc92dcaf53246
GioGreco <grecogiorgio.97@gmail.com>
2022-11-09 16:10:50 +0100

    add jumbotron

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <AppTitle/>
+  <Jumbotrone/>
 </template>
 
 <script>
-import AppTitle from './components/AppTitle.vue'
+import AppTitle from './components/AppTitle.vue';
+import Jumbotrone from './components/Jumbotrone.vue'
 
 export default {
   components: {
-    AppTitle
+    AppTitle,
+    Jumbotrone
   }
 }
 </script>
--- src/components/Jumbotrone.vue
+++ src/components/Jumbotrone.vue
@@ -1,6 +1,9 @@
 <template>
     <div id="jumbo">
-
+        <div class="jumbo-inner-wrap">
+            <h2>Lorem, ipsum.</h2>
+            <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum obcaecati at blanditiis, ad nemo odio incidunt et dicta fugiat in?</div>
+        </div>
     </div>
 </template>
 
@@ -9,12 +12,26 @@
         name: 'Jumbotrone',
         data(){
             return{
-
+                
             }
         }
     }
 </script>
 
 <style lang="css" scoped>
+#jumbo{
+    height: 500px;
+    position: relative;
+    background-image: url('https://artstore.house/wp-content/uploads//2019/10/huge-paintings.jpg');
+    background-size: cover;
+}
 
+.jumbo-inner-wrap{
+    position: absolute;
+    width: 100%;
+    left: 0;
+    bottom: 0;
+    padding: 30px;
+    background-color: rgba(0, 0, 0, 0.5);
+}
 </style>
